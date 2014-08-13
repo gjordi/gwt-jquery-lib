9a02968166d631bbac502ac5fec8d2cb842ff67a
Gjordi McIntyre <gjordi@bytebybyte.com>
2014-08-13 16:45:36 -0600

    Initial commit for jquery 2.1.1

---
+++ jquery-lib/src/main/java/com/bytebybyte/gwt/jquery/client/resources/JQueryLibClientBundle.java
@@ -0,0 +1,14 @@
+package com.bytebybyte.gwt.jquery.client.resources;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+public interface JQueryLibClientBundle extends ClientBundle {
+
+	public final static JQueryLibClientBundle INSTANCE = GWT.create(JQueryLibClientBundle.class);
+
+	@Source("jquery/jquery-2.1.1.min.js")
+	TextResource jquery();
+
+}
